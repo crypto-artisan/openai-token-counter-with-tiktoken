e256b442dc2aacd83aeb1d5119a66305570f3309
hightecular95908 <ronnykutter95908@gmail.com>
2024-02-17 16:49:27 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,58 @@
+
+# let's verify the function above matches the OpenAI API response
+from  num_tokens import num_tokens_from_messages
+from openai import OpenAI
+import os
+openai_api_key = os.environ.get("OPENAI_API_KEY", "<your OpenAI API key if not set as env var>")
+print(openai_api_key)
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "<your OpenAI API key if not set as env var>"))
+
+example_messages = [
+    {
+        "role": "system",
+        "content": "You are a helpful, pattern-following assistant that translates corporate jargon into plain English.",
+    },
+    {
+        "role": "system",
+        "name": "example_user",
+        "content": "New synergies will help drive top-line growth.",
+    },
+    {
+        "role": "system",
+        "name": "example_assistant",
+        "content": "Things working well together will increase revenue.",
+    },
+    {
+        "role": "system",
+        "name": "example_user",
+        "content": "Let's circle back when we have more bandwidth to touch base on opportunities for increased leverage.",
+    },
+    {
+        "role": "system",
+        "name": "example_assistant",
+        "content": "Let's talk later when we're less busy about how to do better.",
+    },
+    {
+        "role": "user",
+        "content": "This late pivot means we don't have time to boil the ocean for the client deliverable.",
+    },
+]
+
+for model in [
+    "gpt-3.5-turbo-0301",
+    "gpt-3.5-turbo-0613",
+    "gpt-3.5-turbo",
+    "gpt-4-0314",
+    "gpt-4-0613",
+    "gpt-4",
+    ]:
+    print(model)
+    # example token count from the function defined above
+    print(f"{num_tokens_from_messages(example_messages, model)} prompt tokens counted by num_tokens_from_messages().")
+    # example token count from the OpenAI API
+    response = client.chat.completions.create(model=model,
+    messages=example_messages,
+    temperature=0,
+    max_tokens=1)
+    print(f'{response.usage.prompt_tokens} prompt tokens counted by the OpenAI API.')
+    print()
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Open AI Token Counter Sample Script
+Generally, token counts vary upon the various model.
+
+[Visit this link for futher details](https://cookbook.openai.com/examples/how_to_count_tokens_with_tiktoken)
